092b65e1b06ead307465d9647bb6a1ec96824107
Damion Wiltshire <drw.5452@gmail.com>
2020-09-18 10:12:14 -0700

    implement routes for auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Call create method on SessionsController class
+  post '/login'  => 'sessions#create'
+
+  # Call destroy method on SessionsController class
+  delete '/logout' => 'sessions#destroy'
+ 
+  # Call profile method on Users class class
+  get '/profile' => 'users#profile'
+  
+  resources :users
+
 end

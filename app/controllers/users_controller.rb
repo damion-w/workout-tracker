dffd8f1deebe71082d873770a854a2352d2ac5d0
Damion Wiltshire <drw.5452@gmail.com>
2020-09-18 10:31:50 -0700

    implement users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+    
+    # Don't do any of this if when a user is registering
+    before_action :require_login, except: [:create]
+
+    # Create user with data from the registration form 
+    def create
+        user = User.create!(user_params)
+        render json: { token: user.auth_token }
+    end
+
+    # Fetch user
+    def profile
+        user = User.find_by_auth_token!(request.headers[:token])
+        render json: { user: { username: user.username, email: user.email, name: user.name } }
+    end
+
+    private
+    
+    def user_params
+        # Specify that only a user object will be accepted with these keys
+        params.require(:user).permit(:username, :email, :password, :name)
+    end
+
+end

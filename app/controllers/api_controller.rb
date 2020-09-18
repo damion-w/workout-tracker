0ff684d8d2ac6c01db85b99c44628278ee1fc114
Damion Wiltshire <drw.5452@gmail.com>
2020-09-18 10:04:42 -0700

    implement sessions and api controllers

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,29 @@
+class ApiController < ApplicationController
+    
+    # If the token is present and valid, proceed with login.  If not, return access denied
+    def require_login
+        authenticate_token || render_unauthorized("Access denied")
+    end
+
+    # If there is a user logged in, return it.  Else, search db for user with this token and return that user
+    def current_user
+        @current_user ||= authenticate_token
+    end
+
+    protected
+
+    # Handles returning login error messages
+    def render_unauthorized(message)
+        errors = { errors: [ detail: message ] }
+        render json: errors, status: :unauthorized
+    end
+
+    private
+
+    # Handles searching for tokens in user table
+    def authenticate_token
+        authenticate_with_http_token do | token, options |
+            User.find_by(auth_token: token)
+        end
+    end
+end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,39 @@
+# Inherit from APiController so that we have access to its methods
+class SessionsController < ApiController
+    
+    # Don't do any of this if when a user is registering
+    skip_before_action :require_login, only: [:create], raise: false
+
+    # If the user enters the correct creds, give them a token.  Else, send an error.
+    def create
+        if user = User.validate_login(params[:username], params[:password])
+            allow_token_to_be_used_only_once_for(user)
+            send_token_for_valid_login_of(user)
+        else
+            render_unauthorized("error with your login or password")
+        end
+    end
+
+    # Called when user logs out
+    def destroy
+        logout
+        head :ok
+    end
+
+    private
+
+    # Generates an auth token
+    def allow_token_to_be_used_only_once_for(user)
+        user.regenerate_auth_token
+    end
+
+    # Sends auth token to client
+    def send_token_for_valid_login_of(user)
+        render json: { token: user.auth_token }
+    end
+
+    # Sets auth_token to nil
+    def logout
+        current_user.invalidate_token
+    end
+end

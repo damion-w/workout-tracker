c68b5d7946658597c605aecbf10844c9616b05b4
Damion Wiltshire <drw.5452@gmail.com>
2020-09-20 20:44:24 -0700

    entering a username and password returns a token

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter, Link, Route } from 'react-router-dom'
+import './App.css'
+import LoginForm from './components/LoginForm'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Exercises!</h1>
-    </div>
-  );
+import Auth from './modules/Auth'
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      auth: Auth.isUserAuthenticated()
+    }
+  }
+  
+  handleLoginSubmit = (evt, values) => {
+    evt.preventDefault()
+
+    console.log(values)
+    fetch('/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      
+      body: JSON.stringify(values)
+    })
+    
+    .then(res => res.json())
+    .then(res => {
+      console.log(res)
+    })
+  }
+
+  render() {
+    console.log(this.state.auth)
+    
+    return(
+      <BrowserRouter>
+        <div className="App">
+          <h1>Exercises!</h1>
+          {this.state.auth ? (
+            <p>You are logged in!</p>
+          ) : (
+            <p>Log in <Link to='/login'>here!</Link></p>
+          )}
+        </div>
+
+        <Route
+          exact
+          path='/login'
+          render={() => <LoginForm handleLoginSubmit={this.handleLoginSubmit} />}
+        />
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
---
+++ client/src/components/LoginForm.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+
+const LoginForm = ({ handleLoginSubmit }) => {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    return (
+      <form onSubmit={(evt) => handleLoginSubmit(evt, { username, password })}>
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          value={username}
+          onChange={(evt) => setUsername(evt.target.value)}
+        />
+
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={password}
+          onChange={(evt) => setPassword(evt.target.value)}
+        />
+
+        <input type='submit' value='Log in' />
+      </form>
+    )
+}
+
+export default LoginForm
